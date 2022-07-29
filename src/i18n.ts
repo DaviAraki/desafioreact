9b5ab11f8eb1407dd4e2c72581da04c3309ee0bc
Davi Araki <davi.araki@gmail.com>
2022-07-29 12:23:22 -0300

    config i18n

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import enUS from "./i18n/enUS.json";
+import ptBR from "./i18n/ptBR.json";
+import esES from "./i18n/esES.json";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  enUS: {
+    translation: enUS,
+  },
+  ptBR: {
+    translation: ptBR,
+  },
+  esES: {
+    translation: esES,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: JSON.parse(localStorage.getItem("language") || '"enUS"'),
+  fallbackLng: "enUS",
+});
+
+export default i18n;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from "react-i18next";
+import useLocalStorage from "../../hooks/useLocalStorage";
 import { LoginButton, LoginContainer, ThemeButton } from "./styles";
 import useViewModel from "./ViewModel";
+import i18n from "../../i18n";
 
 export function LoginPage() {
   const {
@@ -9,7 +12,17 @@ export function LoginPage() {
     onCLickChangeTheme,
   } = useViewModel();
 
+  const { t } = useTranslation();
+
+  const [language, setLanguage] = useLocalStorage("language", "ptBR");
+
   const split = username.split(" ");
+
+  const changeLanguage = (chosenLanguage: string) => {
+    setLanguage(chosenLanguage);
+    i18n.changeLanguage(chosenLanguage);
+  };
+  console.log(language);
 
   return (
     <LoginContainer>
@@ -18,13 +31,17 @@ export function LoginPage() {
           {split[0][0]} {split[1][0]}
         </h1>
       )}
-
+      <h1>{language}</h1>
+      <h1>{t("Settings")}</h1>
       <LoginButton variant="contained" onClick={onButtonSignInClicked}>
         Login
       </LoginButton>
       <ThemeButton variant="contained" onClick={onCLickChangeTheme}>
         Change Theme
       </ThemeButton>
+      <button onClick={() => changeLanguage("enUS")}>enUS</button>
+      <button onClick={() => changeLanguage("ptBR")}>ptBR</button>
+      <button onClick={() => changeLanguage("esES")}>esES</button>
     </LoginContainer>
   );
 }
