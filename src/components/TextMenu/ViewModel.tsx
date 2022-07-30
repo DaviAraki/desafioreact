bb98648cb2cc13f6f1d9a0a7479fa657575b8da7
Davi Araki <davi.araki@gmail.com>
2022-07-30 16:40:59 -0300

    create user info container e ajustar cor de fundo do tema

--- src/components/TextMenu/ViewModel.tsx
+++ src/components/TextMenu/ViewModel.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { MouseEvent, useState } from 'react';
 
 export default function TextMenuViewModel() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
-  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+  const handleClickListItem = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
