786f6ae5d79d69420e2ebba96fdd4c1dff681fff
Davi Araki <davi.araki@gmail.com>
2022-08-01 00:39:21 -0300

    Criar user initial test

---
+++ src/components/UserInitials/index.test.tsx
@@ -0,0 +1,20 @@
+//create unit test for user initials
+
+import { screen } from '@testing-library/react';
+import { UserInitials } from '.';
+import { renderWithContext } from '../../utils/test-utils';
+
+describe('UserInitials', () => {
+  it('renders and have the correct style for medium', () => {
+    renderWithContext(<UserInitials size={'medium'}>AB</UserInitials>);
+    const initials = screen.getByText('AB');
+    expect(initials).toBeInTheDocument();
+    expect(initials).toHaveStyle({ fontSize: '1rem' });
+  });
+  it('renders and have the correct style for small', () => {
+    renderWithContext(<UserInitials size={'small'}>DA</UserInitials>);
+    const initials = screen.getByText('DA');
+    expect(initials).toBeInTheDocument();
+    expect(initials).toHaveStyle({ fontSize: '0.8rem' });
+  });
+});
