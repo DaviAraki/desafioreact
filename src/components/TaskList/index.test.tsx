f5bb8ab9a6a37ec89fecc55fc9ca8f2accedd2c5
Davi Araki <davi.araki@gmail.com>
2022-08-01 02:42:50 -0300

    improved home test

--- src/components/TaskList/index.test.tsx
+++ src/components/TaskList/index.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import TaskList from '.';
 import { renderWithContext } from '../../utils/test-utils';
 
